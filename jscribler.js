bdb4db749d19e21960dd065c0b0bb51fcb91f901
Tanish Sharma <70182737+Tanish118@users.noreply.github.com>
2021-03-27 18:47:38 +0530

    changes

--- jscribler.js
+++ jscribler.js
@@ -1,51 +1,61 @@
- // Get the modal
+ // sign in
         var modal = document.getElementById("myModal");
 
-        // Get the button that opens the modal
         var btn = document.getElementById("myBtn");
 
-        // Get the <span> element that closes the modal
         var span = document.getElementsByClassName("close")[0];
 
-        // When the user clicks the button, open the modal
         btn.onclick = function () {
             modal.style.display = "block";
         }
-
-        // When the user clicks on <span> (x), close the modal
         span.onclick = function () {
             modal.style.display = "none";
         }
-
-        // When the user clicks anywhere outside of the modal, close it
         window.onclick = function (event) {
             if (event.target == modal) {
                 modal.style.display = "none";
             }
 }
 
-//sgn in
+//sgn up
 var modal1 = document.getElementById("sModal");
 
-// Get the button that opens the modal
 var btn1 = document.getElementById("Btn");
 
-// Get the <span> element that closes the modal
 var span1 = document.getElementsByClassName("cross")[0];
 
-// When the user clicks the button, open the modal
 btn1.onclick = function () {
     modal1.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span1.onclick = function () {
     modal1.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function (event) {
     if (event.target == modal1) {
         modal1.style.display = "none";
+    }
+}
+
+
+// Create Post
+var txt = document.getElementById("modal-context");
+
+var create = document.getElementById("createPost");
+
+var c = document.getElementsByClassName("closed")[0];
+
+create.onclick = function () {
+    txt.style.display = "block";
+}
+
+c.onclick = function () {
+    txt.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == txt) {
+        txt.style.display = "none";
     }
 }
