d6fc83a0792dc591629fed3dabed8f9325673bde
Tanish Sharma <70182737+Tanish118@users.noreply.github.com>
2021-03-28 08:37:44 +0530

    page2 Completed

--- script/bloglist.js
+++ script/bloglist.js
@@ -1,1 +1,20 @@
 // JavaScript source code
+var configuration_model = document.getElementsByClassName("Confirmation_box_model")[0];
+
+var configuration_btn = document.querySelectorAll(".trash");
+
+var configuration_no = document.getElementById("no");
+
+
+
+var j;
+for (j = 0; j < configuration_btn.length; j++) {
+    let btn = configuration_btn[j];
+    btn.onclick = function () {
+        configuration_model.style.display = "flex";
+    }
+}
+
+configuration_no.onclick = function () {
+    configuration_model.style.display = "none";
+}
